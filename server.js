e45a4725db227937c670a3b800c2292a40292d35
Denise Fernandez <dcbfernandez@gmail.com>
2014-06-17 16:28:19 -0400

    Writing some unit tests for the server

--- server.js
+++ server.js
@@ -4,32 +4,47 @@ var fs = require('fs');
 var DAY = "day";
 var NIGHT = "night";
 
+var contentTypes = {"js" : "application/javascript", "html" : "text/html", "json" : "application/json"};
+
+
+
 var server = {
-  requestHandler: function (req, res) {
-    if (req.url === "/") {
-      fs.readFile(__dirname + '/index.html', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(data);
-      });
-    } else if (req.url === "/client.js") {
-      fs.readFile(__dirname + '/client.js', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/javascript' });
-        res.end(data);
-      });
-    } else if (req.url === "/canvas-renderer.js") {
-      fs.readFile(__dirname + '/canvas-renderer.js', 'utf8', function (err, data) {
-        res.writeHead(200, { 'Content-Type': 'application/javascript' });
-        res.end(data);
-      });
-    } else if (req.url === "/time.json") {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
+    requestHandler: function (req, res) {
+        if (req.url === "/") {
+            this.readFile("/index.html", res);
+        } else if (req.url === "/client.js") {
+            this.readFile(req.url, res);
+        } else if (req.url === "/canvas-renderer.js") {
+            this.readFile(req.url, res);
+        } else if (req.url === "/time.json") {
+            res.writeHead(200, { 'Content-Type': 'application/json' });
+            var timeOfDay = this.getTimeOfDay();
+            res.end(this.createJSON("time", timeOfDay));
+        }
+    },
+    readFile: function(path, res) {
+        var ext = this.getFileExtension(path);
+        fs.readFile(__dirname + path, 'utf8', function (err, data) {
+            res.writeHead(200, { 'Content-Type': contentTypes[ext] });
+            res.end(data);
+        });
+    },
+    getFileExtension: function(path) {
+        console.log("path %s", path);
+        var ext = path.substring(path.indexOf(".") + 1, path.length);
+        console.log(ext);
+        return ext;
+    },
+    getTimeOfDay: function() {
+        var hour = new Date().getHours();
+        console.log("###" + hour)
+        var time = hour > 6 && hour < 20 ? DAY : NIGHT;
 
-      var hour = new Date().getHours();
-      var time = hour > 6 && hour < 20 ? DAY : NIGHT;
-
-      res.end('{ "time": "' + time + '" }');
+        return time;
+    },
+    createJSON: function(key, value) {
+        return '{ ' + key + ': "' + value + '" }'
     }
-  }
 };
 
 http.createServer(server.requestHandler).listen(4000);
--- tests/server_tests.js
+++ tests/server_tests.js
@@ -3,28 +3,66 @@ var server = require('../server').server;
 var http = require('http');
 
 var mockRes = function(expectedString, done) {
-  http.ServerResponse.prototype.end = function(data) {
-    tester.isEqual(data.substring(0, expectedString.length),
-                   expectedString);
-    console.log("assertion checked");
+    http.ServerResponse.prototype.end = function(data) {
+        tester.isEqual(data.substring(0, expectedString.length),
+            expectedString);
+        console.log("assertion checked");
 
-    if (done !== undefined) {
-      done();
-    }
-  };
+        if (done !== undefined) {
+            done();
+        }
+    };
 
-  return new http.ServerResponse({});
+    http.ServerResponse.prototype.writeHead = function(statusCode, responseHeaders) {
+    };
+
+    return new http.ServerResponse({});
 };
+
+var mockFs = function() {
+    this.readFile = function(path, encoding) {
+        console.log("read file");
+        return "some file stuff";
+    };
+}
 
 // Demo async tests
 tester.test(
-  'should serve index.html when go to "/" URL', function(done) {
-    server.requestHandler({ url: "/" },
-                          mockRes("<!doctype", done));
-  },
 
-  'should serve client.js when go to "/client.js" URL', function(done) {
-    server.requestHandler({ url: "/client.js" },
-                          mockRes(";(function(exports) {\n  exports.get", done));
-  }
+    'should get file extension form path', function() {
+        var result = server.getFileExtension("/client.js");
+        tester.isEqual(result, "js");
+        console.log("successass")
+    },
+
+    'should serve index.html when go to "/" URL', function(done) {
+        server.requestHandler({ url: "/" },
+            mockRes("<!doctype", done));
+    },
+
+    'should serve client.js when go to "/client.js" URL', function(done) {
+        server.requestHandler({ url: "/client.js" },
+            mockRes(";(function(exports) {\n  exports.get", done));
+    },
+
+    'should get day string when hour between 6 and 20 (exclusive)', function() {
+        // mock
+        var Date = function() {
+            this.getHours = function() {
+                return 11;
+            }
+        }
+
+        result = server.getTimeOfDay();
+        tester.isEqual(result, "day");
+    },
+
+    'should read file with given path', function(done) {
+        server.readFile("/client.js", mockRes(";(function(exports) {\n  exports.get", done));
+    }
+
+//  'should create json from key and value', function() {
+//        result = server.createJSON("time", "day");
+//        tester.isEqual(result, '{"time"}')
+//    }
 );
